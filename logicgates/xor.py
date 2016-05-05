0e1ee9b3affdc0f4107ef4860dff7c6270f89b5c
Kwan-Yuet Ho, Ph.D <stephenhky@yahoo.com.hk>
2016-05-05 15:52:23 -0400

    working xor gate

--- logicgates/xor.py
+++ logicgates/xor.py
@@ -4,15 +4,16 @@ from keras.models import Model
 import numpy as np
 
 data = np.array([[0., 0.], [0., 1.], [1., 0.], [1., 1.]])
-labels = np.array([[0.], [1.], [1.], [0.]])
+labels = np.array([[0., 1.], [1., 0.], [1., 0.], [1., 0.]])
 
 def xor_gate():
     inputs = Input(shape=(2,))
-    x = Dense(2, activation='relu')(inputs)
-    predictions = Dense(1, activation='softmax')(x)
+    x = Dense(1024, activation='relu')(inputs)
+    x = Dense(2048, activation='relu')(x)
+    predictions = Dense(2, activation='softmax')(x)
 
     model = Model(input=inputs, output=predictions)
-    model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+    model.compile(optimizer='rmsprop', loss='mean_squared_error', metrics=['accuracy'])
     model.fit(data, labels)
 
     return model
